d7e39203628ece416ecff17302d1df9e0feaf7c7
“Daniel” <daniel.fabian13x@gmail.com>
2022-12-13 20:28:59 +0100

    Update script and APK

--- compile-apk.bat
+++ compile-apk.bat
@@ -1,12 +1,13 @@
 @echo off
 
-cd ./android
-
-.\gradlew.bat assembleDebug
-
+cd .\android
+.\gradlew.bat assembleDebug | echo>nul
 cd ..
 
-set debug-apk-path="./android/app/build/outputs/apk/debug/app-debug.apk"
-set output-apk-path="./TaskManager.apk"
 
-copy /y %debug-apk-path% %output-apk-path%
+
+
+set debug-apk-path=".\android\app\build\outputs\apk\debug\app-debug.apk"
+set output-apk-path=".\TaskManager.apk"
+
+copy %debug-apk-path% %output-apk-path%
